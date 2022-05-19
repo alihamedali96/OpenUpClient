ccca08df99f305927e434d916dd43c075e521362
“Igormirowski” <igorpmirowski@gmail.com>
2022-05-19 09:32:47 +0100

    style darkmode

--- myblog.js
+++ myblog.js
@@ -190,3 +190,33 @@ const submit = document.getElementById("submit");
 
 submit.addEventListener("click", postBlog)
 // submitprivate.addEventListener("click", postBlog);
+
+
+
+
+///////DARK MODE/////////
+
+const about = document.getElementById("aboutLink");
+const darkmode = document.getElementById("darkLink");
+let CheckDarkOn = false;
+
+darkmode.addEventListener("click", swicthColours);
+
+function swicthColours(e) {
+  console.log("dark mode clicked");
+  console.log(CheckDarkOn);
+  e.preventDefault();
+  let mainbody = document.querySelector("body");
+  //   mainbody.style.backgroundColor = "rgb(39, 39, 39)";
+  //   mainbody.style.color = "white";
+
+  if (CheckDarkOn === false) {
+    mainbody.style.backgroundColor = "rgb(39, 39, 39)";
+    mainbody.style.color = "white";
+    CheckDarkOn = true;
+  } else {
+    mainbody.style.backgroundColor = "white";
+    mainbody.style.color = "black";
+    CheckDarkOn = false;
+  }
+}
